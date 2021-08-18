bd580d721d4c3c887996f3f31ad758ed4ba447e3
avatarp <msiedlecki5@gmail.com>
2021-08-18 18:14:23 +0200

    Initial commit

---
+++ utils.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include <sstream>
+#include <QtDebug>
+#include <sstream>
+#include <iomanip>
+#include <ctime>
+#include "constants.h"
+namespace utils {
+long hexToDec(std::string hex);
+std::string hexToBin(std::string hex);
+std::string decToHex(int dec);
+void removeControlCharacters(std::string &text);
+void removeControlCharacters(char *text, int size);
+std::string getTimestamp();
+}
