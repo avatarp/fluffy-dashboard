d4f077760e669cb64371e61db07f5a3f7bae2cbe
Mateusz Siedlecki <msiedlecki5@gmail.com>
2023-07-05 22:07:24 +0200

    #11 refactor usb provider

--- Source/fluffy-obd-lib/obd-access/usb-serial-provider.cpp
+++ Source/fluffy-obd-lib/obd-access/usb-serial-provider.cpp
@@ -1,45 +1,44 @@
 #include "usb-serial-provider.hpp"
 namespace Obd {
 
-
-std::vector<Obd::Device> Obd::UsbSerialProvider::GetAvailableDevices()
-{
-    std::vector<Obd::Device> DevicesFound;
-    std::string response = SystemCallForResponse("ls /dev/serial/by-id");
-    if(response.size()==0)
-        return DevicesFound;
-
-    std::vector<std::string> deviceFiles = std::vector<std::string>{};
-    auto ss = std::stringstream{response};
-
-
-    std::string getDeviceInfoCommand = "udevadm info /dev/serial/by-id/";
-
-    for (std::string deviceFile; std::getline(ss, deviceFile, '\n');)
+    std::vector<Obd::Device> Obd::UsbSerialProvider::GetAvailableDevices()
     {
-        std::string deviceInfo = SystemCallForResponse((getDeviceInfoCommand+deviceFile).c_str());
-        std::regex DeviceFilePathPattern("N: (.+)\n");
-        std::smatch Match;
-        if(std::regex_search(deviceInfo, Match, DeviceFilePathPattern))
+        std::vector<Obd::Device> devicesFound;
+        std::string response = SystemCallForResponse("ls /dev/serial/by-id");
+        if (response.empty())
         {
-            std::string deviceFilePath = Match[1];
+            return devicesFound;
+        }
+        std::vector<std::string> deviceFiles = std::vector<std::string>{};
+        auto sstream = std::stringstream{response};
 
-            std::regex DeviceVendorPattern("E: ID_VENDOR_FROM_DATABASE=(.+)\n");
-            if(std::regex_search(deviceInfo, Match, DeviceVendorPattern))
+        std::string getDeviceInfoCommand = "udevadm info /dev/serial/by-id/";
+
+        for (std::string deviceFile; std::getline(sstream, deviceFile, '\n');)
+        {
+            std::string deviceInfo = SystemCallForResponse((getDeviceInfoCommand + deviceFile).c_str());
+            std::regex deviceFilePathPattern("N: (.+)\n");
+            std::smatch match;
+            if (std::regex_search(deviceInfo, match, deviceFilePathPattern))
             {
-                std::string deviceVendor = Match[1];
+                std::string deviceFilePath = match[1];
 
-                std::regex DeviceModelPattern("E: ID_MODEL_FROM_DATABASE=(.+)\n");
-                if(std::regex_search(deviceInfo, Match, DeviceModelPattern))
+                std::regex deviceVendorPattern("E: ID_VENDOR_FROM_DATABASE=(.+)\n");
+                if (std::regex_search(deviceInfo, match, deviceVendorPattern))
                 {
-                    std::string deviceModel = Match[1];
-                    std::string deviceName = deviceVendor + " " + deviceModel;
-                    DevicesFound.push_back(Obd::Device(deviceFilePath,Obd::ConnectionType::Usb, deviceName));
+                    std::string deviceVendor = match[1];
+
+                    std::regex deviceModelPattern("E: ID_MODEL_FROM_DATABASE=(.+)\n");
+                    if (std::regex_search(deviceInfo, match, deviceModelPattern))
+                    {
+                        std::string deviceModel = match[1];
+                        std::string deviceName = deviceVendor.append(" ").append(deviceModel);
+                        devicesFound.emplace_back(Obd::Device(deviceFilePath, Obd::ConnectionType::Usb, deviceName));
+                    }
                 }
             }
         }
+        std::cout << "Found " << devicesFound.size() << " devices!" << std::endl;
+        return devicesFound;
     }
-    std::cout<<"Found "<<DevicesFound.size()<<" devices!"<<std::endl;
-    return DevicesFound;
-}
-}
+} // namespace Obd
