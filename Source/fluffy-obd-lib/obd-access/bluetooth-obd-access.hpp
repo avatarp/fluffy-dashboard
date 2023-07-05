47ec0780519943a1a1e57860c7ede972c85a3bbf
Mateusz Siedlecki <msiedlecki5@gmail.com>
2023-07-05 22:11:56 +0200

    #11 refactor bt access

--- Source/fluffy-obd-lib/obd-access/bluetooth-obd-access.hpp
+++ Source/fluffy-obd-lib/obd-access/bluetooth-obd-access.hpp
@@ -9,6 +9,12 @@ private:
     virtual bool Write(const std::string& command) override;
     virtual std::string Read() override;
     void SetupDefaultTermios();
+
+    /// @brief Read timeout in 0.1s
+    static constexpr uint8_t timeout= 10;
+    /// @brief Time Blocking read between characters in 0.1s
+    static constexpr uint8_t readBlockingInterval = 5;
+
 public:
     BluetoothObdAccess() = default;
     ~BluetoothObdAccess() = default;
