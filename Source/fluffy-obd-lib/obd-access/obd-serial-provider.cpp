41db8956776e8d858c27717cccd7bec671b74a76
Mateusz Siedlecki <msiedlecki5@gmail.com>
2024-06-02 20:56:39 +0200

    #40 Fix clang warnings in obd-serial-provider

--- Source/fluffy-obd-lib/obd-access/obd-serial-provider.cpp
+++ Source/fluffy-obd-lib/obd-access/obd-serial-provider.cpp
@@ -12,20 +12,20 @@ std::vector<Obd::Device> Obd::ObdSerialProvider::GetAvailableDevices()
         if (!exists(serialDevicesPath)) {
             std::cout << serialDevicesPath.generic_string() << " does not exist\n";
             return devices;
-        } else {
-            for (auto devicePath : fs::directory_iterator(serialDevicesPath)) {
-                if (is_symlink(devicePath.symlink_status())) {
+        }
+        for (const auto& devicePath : fs::directory_iterator(serialDevicesPath)) {
+            if (is_symlink(devicePath.symlink_status())) {
 
-                    std::string canonical_path = fs::canonical(
-                        serialDevicesPath / read_symlink(devicePath))
-                                                     .generic_string();
+                std::string canonicalPath = fs::canonical(
+                    serialDevicesPath / read_symlink(devicePath))
+                                                .generic_string();
 
-                    devices.push_back(Obd::Device(canonical_path,
-                        Obd::ConnectionType::Serial,
-                        devicePath.path().filename()));
-                }
+                devices.emplace_back(Obd::Device(canonicalPath,
+                    Obd::ConnectionType::Serial,
+                    devicePath.path().filename()));
             }
         }
+
     } catch (const fs::filesystem_error& ex) {
         std::cout << ex.what() << '\n';
         return devices;
