88272c6724be73e29ba35f806f1091d80c697e5b
Mateusz Siedlecki <msiedlecki5@gmail.com>
2024-06-23 08:44:50 +0200

    #52 Remove -Weffc++ flag

--- Source/cmake/Warnings.cmake
+++ Source/cmake/Warnings.cmake
@@ -32,7 +32,6 @@ function(target_set_warnings)
         -Wnon-virtual-dtor # if a class with virtual func has a non-virtual dest
         -Wold-style-cast # warn for c-style casts
         -Woverloaded-virtual # if you overload (not override) a virtual function
-        -Weffc++ # violations from Scott Meyers’ Effective C++
     )
 
     set(GCC_WARNINGS
