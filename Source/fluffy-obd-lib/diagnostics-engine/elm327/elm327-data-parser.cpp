58810d9cbd2ec70134cd12a5377019ebe49cb6ca
Mateusz Siedlecki <msiedlecki5@gmail.com>
2024-11-20 21:05:08 +0100

    #61 Add response size helper function

--- Source/fluffy-obd-lib/diagnostics-engine/elm327/elm327-data-parser.cpp
+++ Source/fluffy-obd-lib/diagnostics-engine/elm327/elm327-data-parser.cpp
@@ -33,7 +33,82 @@ RawResponse Elm327DataParser::ParseResponse(std::string data,
         }
         return parsedResponse;
     }
-    throw(std::runtime_error { "Response to: "
-        + commandPid.substr(0, commandPid.size() - 1)
-        + ", not matched!\n" });
+
+std::size_t Elm327DataParser::getExpectedResponseSizeByPid(ObdCommandPid pid)
+{
+    using enum ObdCommandPid;
+    switch (pid) {
+    case S01P04:
+    case S01P05:
+    case S01P06:
+    case S01P07:
+    case S01P08:
+    case S01P09:
+    case S01P0A:
+    case S01P0B:
+    case S01P0D:
+    case S01P0E:
+    case S01P0F:
+    case S01P11:
+    case S01P12:
+    case S01P13:
+    case S01P1C:
+    case S01P1D:
+    case S01P1E:
+    case S01P2C:
+    case S01P2D:
+    case S01P2E:
+    case S01P2F:
+    case S01P30:
+    case S01P33:
+        return 1;
+
+    case S01P02:
+    case S01P03:
+    case S01P0C:
+    case S01P10:
+    case S01P14:
+    case S01P15:
+    case S01P16:
+    case S01P17:
+    case S01P18:
+    case S01P19:
+    case S01P1A:
+    case S01P1B:
+    case S01P1F:
+    case S01P21:
+    case S01P22:
+    case S01P23:
+    case S01P31:
+    case S01P32:
+    case S01P3C:
+    case S01P3D:
+    case S01P3E:
+    case S01P3F:
+        return 2;
+
+    case S01P00:
+    case S01P01:
+    case S01P20:
+    case S01P24:
+    case S01P25:
+    case S01P26:
+    case S01P27:
+    case S01P28:
+    case S01P29:
+    case S01P2A:
+    case S01P2B:
+    case S01P34:
+    case S01P35:
+    case S01P36:
+    case S01P37:
+    case S01P38:
+    case S01P39:
+    case S01P3B:
+
+    case S0900:
+        return 4;
+    default:
+        return 0;
+    }
 }
