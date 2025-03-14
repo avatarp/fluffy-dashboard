54d42e47ec15fa1406fad2bb9e84630c8972dd90
Mateusz Siedlecki <msiedlecki5@gmail.com>
2025-03-14 10:29:48 +0100

    #59 Move response decoding to decoder

--- Source/fluffy-obd-lib/diagnostics-engine/elm327/elm327-data-decoder.cpp
+++ Source/fluffy-obd-lib/diagnostics-engine/elm327/elm327-data-decoder.cpp
@@ -1,6 +1,357 @@
 #include "elm327-data-decoder.hpp"
 #include "elm327-decoders.hpp"
 
+void Elm327DataDecoder::decodeResponse(Response &response)
+{
+    using enum ObdCommandPid;
+
+    switch (response.commandPid) {
+    case S01P00:
+        response.m_dataType = DataType::bitset;
+        response.m_dataBitset = this->GetSupportedPIDs1(response.m_rawData);
+        return;
+    case S01P01:
+        response.m_dataType = DataType::bitset;
+        response.m_dataBitset = this->GetMonitorStatus(response.m_rawData);
+        return;
+    case S01P02:
+        response.m_dataType = DataType::string;
+        response.m_dataString = std::pair<std::string, std::string>(
+            this->GetFreezeDTCs(response.m_rawData), "FreezeDTCs");
+        return;
+    case S01P03:
+        response.m_dataType = DataType::bitset;
+        response.m_dataBitset = this->GetFuelSystemStatus(response.m_rawData);
+        return;
+    case S01P04:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetEngineLoad(response.m_rawData), "%" };
+        return;
+    case S01P05:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetEngineCoolantTemperature(response.m_rawData), "°C" };
+        return;
+    case S01P06:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetShortTermFuelTrimBank1(response.m_rawData), "%" };
+        return;
+    case S01P07:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetLongTermFuelTrimBank1(response.m_rawData), "%" };
+        return;
+    case S01P08:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetShortTermFuelTrimBank2(response.m_rawData), "%" };
+        return;
+    case S01P09:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetLongTermFuelTrimBank2(response.m_rawData), "%" };
+        return;
+    case S01P0A:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetFuelPressure(response.m_rawData), "kPa" };
+        return;
+    case S01P0B:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetIntakeManifoldPressure(response.m_rawData), "kPa" };
+        return;
+    case S01P0C:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetEngineRpm(response.m_rawData), "rpm" };
+        return;
+    case S01P0D:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetSpeed(response.m_rawData), "km/h" };
+        return;
+    case S01P0E:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetTimingAdvance(response.m_rawData), "° before TDC" };
+        return;
+    case S01P0F:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetIntakeAirTemperature(response.m_rawData), "°C" };
+        return;
+    case S01P10:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetMafAirFlowRate(response.m_rawData), "g/s" };
+        return;
+    case S01P11:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetThrottlePosition(response.m_rawData), "%" };
+        return;
+    case S01P12:
+        response.m_dataType = DataType::bitset;
+        response.m_dataBitset = this->GetSecondaryAirStatus(response.m_rawData);
+        return;
+    case S01P13:
+        response.m_dataType = DataType::bitset;
+        response.m_dataBitset = this->GetOxygenSensorsPresent1(response.m_rawData);
+        return;
+    case S01P14: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorFtV1(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "V" };
+        response.m_dataFloat2 = { decodedPair.second, "%" };
+        return;
+    }
+    case S01P15: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorFtV2(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "V" };
+        response.m_dataFloat2 = { decodedPair.second, "%" };
+        return;
+    }
+    case S01P16: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorFtV3(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "V" };
+        response.m_dataFloat2 = { decodedPair.second, "%" };
+        return;
+    }
+    case S01P17: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorFtV4(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "V" };
+        response.m_dataFloat2 = { decodedPair.second, "%" };
+        return;
+    }
+    case S01P18: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorFtV5(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "V" };
+        response.m_dataFloat2 = { decodedPair.second, "%" };
+        return;
+    }
+    case S01P19: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorFtV6(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "V" };
+        response.m_dataFloat2 = { decodedPair.second, "%" };
+        return;
+    }
+    case S01P1A: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorFtV7(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "V" };
+        response.m_dataFloat2 = { decodedPair.second, "%" };
+        return;
+    }
+    case S01P1B: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorFtV8(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "V" };
+        response.m_dataFloat2 = { decodedPair.second, "%" };
+        return;
+    }
+    case S01P1C:
+        response.m_dataType = DataType::bitset;
+        response.m_dataBitset = this->GetObdStandard(response.m_rawData);
+        return;
+    case S01P1D:
+        response.m_dataType = DataType::bitset;
+        response.m_dataBitset = this->GetOxygenSensorsPresent2(response.m_rawData);
+        return;
+    case S01P1E:
+        response.m_dataType = DataType::bitset;
+        response.m_dataBitset = this->GetAuxiliaryInputStatus(response.m_rawData);
+        return;
+    case S01P1F:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetRunTime(response.m_rawData), "s" };
+        return;
+    case S01P20:
+        response.m_dataType = DataType::bitset;
+        response.m_dataBitset = this->GetSupportedPIDs2(response.m_rawData);
+        return;
+    case S01P21:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetDistanceWithMilOn(response.m_rawData), "km" };
+        return;
+    case S01P22:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetFuelRailPressure(response.m_rawData), "kPa" };
+        return;
+    case S01P23:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetFuelRailGaugePressure(response.m_rawData), "kPa" };
+        return;
+    case S01P24: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqV1(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "V" };
+        return;
+    }
+    case S01P25: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqV2(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "V" };
+        return;
+    }
+    case S01P26: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqV3(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "V" };
+        return;
+    }
+    case S01P27: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqV4(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "V" };
+        return;
+    }
+    case S01P28: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqV5(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "V" };
+        return;
+    }
+    case S01P29: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqV6(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "V" };
+        return;
+    }
+    case S01P2A: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqV7(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "V" };
+        return;
+    }
+    case S01P2B: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqV8(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "V" };
+        return;
+    }
+    case S01P2C:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetCommandedEgr(response.m_rawData), "%" };
+        return;
+    case S01P2D:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetEgrError(response.m_rawData), "%" };
+        return;
+    case S01P2E:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetCommandedEvaporativePurge(response.m_rawData), "%" };
+        return;
+    case S01P2F:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetFuelTankLevelInput(response.m_rawData), "%" };
+        return;
+    case S01P30:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetWarmupsSinceDtcCleared(response.m_rawData), "count" };
+        return;
+    case S01P31:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetDistanceSinceDtcCleared(response.m_rawData), "km" };
+        return;
+    case S01P32:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetEvapVaporPressure(response.m_rawData), "Pa" };
+        return;
+    case S01P33:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1 = { this->GetAbsoluteBarometricPressure(response.m_rawData), "kPa" };
+        return;
+    case S01P34: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqC1(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "mA" };
+        return;
+    }
+    case S01P35: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqC2(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "mA" };
+        return;
+    }
+    case S01P36: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqC3(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "mA" };
+        return;
+    }
+    case S01P37: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqC4(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "mA" };
+        return;
+    }
+    case S01P38: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqC5(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "mA" };
+        return;
+    }
+    case S01P39: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqC6(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "mA" };
+        return;
+    }
+    case S01P3A: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqC7(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "mA" };
+        return;
+    }
+    case S01P3B: {
+        response.m_dataType = DataType::numberPair;
+        auto decodedPair = this->GetOxygenSensorEqC8(response.m_rawData);
+        response.m_dataFloat1 = { decodedPair.first, "ratio" };
+        response.m_dataFloat2 = { decodedPair.second, "mA" };
+        return;
+    }
+    case S01P3C: {
+        response.m_dataType = DataType::numberPair;
+        response.m_dataFloat1
+            = { this->GetCatalystTemperatureB1S1(response.m_rawData), "°C" };
+        return;
+    }
+    case S01P3D:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1
+            = { this->GetCatalystTemperatureB2S1(response.m_rawData),
+                  "°C" };
+        return;
+    case S01P3E:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1
+            = { this->GetCatalystTemperatureB1S2(response.m_rawData),
+                  "°C" };
+        return;
+    case S01P3F:
+        response.m_dataType = DataType::number;
+        response.m_dataFloat1
+            = { this->GetCatalystTemperatureB2S2(response.m_rawData),
+                  "°C" };
+        return;
+
+    case S09P00:
+        response.m_dataType = DataType::bitset;
+        response.m_dataBitset = this->GetSupportedVIPIDs(response.m_rawData);
+    default:
+        return;
+    }
+}
+
 bitset_32 Elm327DataDecoder::GetSupportedPIDs1(const std::string& data)
 {
     return elm327::decoders::DecodeBitEncoded(data);
