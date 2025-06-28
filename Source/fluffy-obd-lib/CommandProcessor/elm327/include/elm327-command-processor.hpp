23c36511e7086a79c99d7860fdc5509cdd727008
Mateusz Siedlecki <msiedlecki5@gmail.com>
2025-06-28 20:45:01 +0200

    #82 Refactor elm327 engine into command processor

---
+++ Source/fluffy-obd-lib/CommandProcessor/elm327/include/elm327-command-processor.hpp
@@ -0,0 +1,23 @@
+#ifndef ELM327_COMMAND_PROCESSOR_HPP_
+#define ELM327_COMMAND_PROCESSOR_HPP_
+
+#include "ICommandProcessor.hpp"
+#include "elm327-command-repository.hpp"
+#include "elm327-data-decoder.hpp"
+#include "elm327-data-parser.hpp"
+
+class Elm327CommandProcessor : public ICommandProcessor {
+public:
+    Elm327CommandProcessor()
+        : ICommandProcessor(
+            std::make_shared<Elm327DataDecoder>(),
+            std::make_shared<Elm327DataParser>(),
+            std::make_shared<Elm327CommandRepository>())
+    {
+    }
+    virtual ~Elm327CommandProcessor() = default;
+
+    Response GetCommandResponse(ObdCommandPid pid) override;
+};
+
+#endif // ELM327_COMMAND_PROCESSOR_HPP_
